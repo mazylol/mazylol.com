ce193c3f23e01cfcc9cdbc467035c7bff977c6f9
mazylol <landonp95@gmail.com>
2023-05-07 00:31:00 -0400

    all done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<header>
+	<h1>Mazylol</h1>
+	<aside>My website doesn't need to be flashy to be effective!</aside>
+</header>
+
+<section>
+	<h2>Tiny Bit About Me</h2>
+	<p>
+		I am a 16 year old high student that likes to do random stuff that I think could be slightly
+		interesting. I write some of the worst code imaginable. Don't belive me? Check out my <a
+			href="https://github.com/mazylol">Github</a
+		>
+	</p>
+</section>
+
+<section>
+	<h2>Stuff I Do</h2>
+	<ul>
+		<li>Discord Bots</li>
+		<li>API Development</li>
+		<li>
+			Games with <a href="https://github.com/raysan5/raylib">Raylib</a> (plus the rust
+			<a href="https://github.com/deltaphc/raylib-rs">bindings</a>)
+		</li>
+		<li>*NIX Sysadmin</li>
+	</ul>
+</section>
+
+<section>
+	<h2>Languages I Like</h2>
+	<ul>
+		<li>C</li>
+		<li>C#</li>
+		<li>Rust</li>
+		<li>Zig</li>
+		<li>Java</li>
+		<li>Go</li>
+	</ul>
+</section>
+
+<section>
+	<h2>Tools I Like</h2>
+	<ul>
+		<li>Kitty/Alacritty</li>
+		<li>NeoVim</li>
+		<li>Jetbrains Software</li>
+	</ul>
+</section>
+
+<section>
+	<h2>Links</h2>
+	<ul>
+		<li><a href="https://github.com/mazylol">Github</a></li>
+		<li><a href="https://twitter.com/mazylol">Twitter</a></li>
+	</ul>
+</section>
+
+<hr />
+
+<p>
+	<i
+		>Yes, I am using the <a href="http://bettermotherfuckingwebsite.com/"
+			>bettermotherfuckingwebsite.com</a
+		> stylesheet. The only difference is that I am using SvelteKit instead of raw HTML. I plan on using
+		markdown for a blog and SvelteKit + MDSVEX makes that somewhat easy.</i
+	>
+</p>
