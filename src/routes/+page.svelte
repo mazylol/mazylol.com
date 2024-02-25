cb30bdeb0fe350cc2b8f6ab73e4713b579806fdb
Landon <landonp95@gmail.com>
2024-02-24 22:33:35 -0500

    added project links, and a little hidden surprise

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,20 +20,34 @@
 <section>
 	<h2>Tiny Bit About Me</h2>
 	<p>
-		I am a <b>{getAge('2006-09-05')}</b> year old high student that likes to do random stuff that I
-		think could be slightly interesting. I write some of the worst code imaginable. Don't belive me?
-		Check out my <a href="https://github.com/mazylol">GitHub</a>
-	</p>
+		I am a <b>{getAge('2006-09-05')}</b> year old student that likes to do random stuff that I find interesting. I cursed myself with this hobby/obsession while in quarantine when I made the mistake of learning Python. Now here we are.</p>
 </section>
 
 <section>
 	<h2>Stuff I Do</h2>
 	<ul>
 		<li>Discord Bots</li>
-		<li>APIs</li>
+            <ul>
+                <li><a href="https://github.com/mazylol/kodi">Kodi</a> (programming info bot) (C++)</li>
+                <li><a href="https://github.com/mazylol/fido">Fido</a> (random dog pictures) (Rust)</li>
+                <li><a href="https://github.com/mazylol/buildabot">Buildabot</a> (build a bot in completely in yaml) (C++)</li>
+            </ul>
 		<li>Libraries</li>
-		<li>Embedded Software</li>
+            <ul>
+                <li><a href="https://github.com/mazylol/QuickLED">QuickLED</a>, a platformio library to make using LEDs easier</li>
+                <li><a href="https://github.com/mazylol/ultimate-bogo">ultimate-bogo</a>, a multithreaded bogosort implementation in C++ (high risk, high reward)</li>
+                <li>A lot of API deserialization libraries. Usually in Go or C++
+            </ul>
 		<li>*NIX</li>
+            <ul>
+                <li><a href="https://github.com/mazylol/dwm">dwm</a> config</li>
+                <li><a href="https://github.com/mazylol/dmenu">dmenu</a> config</li>
+                <li><a href="https://github.com/mazylol/slstatus">slstatus</a> config</li>
+            </ul>
+        <li>3D Modeling/Printing</li>
+            <ul>
+                <li>I designed a ketchup centrifuge to get all of the ketchup off of the walls of the bottle. Spins violently via a DC motor.
+            </ul>
 	</ul>
 </section>
 
@@ -41,20 +55,18 @@
 	<h2>Languages I Like</h2>
 	<ul>
 		<li>C/C++</li>
+        <li>Go</li>
 		<li>Rust</li>
-		<li>Go</li>
-		<li>Java (sometimes)</li>
-		<li>Typescript</li>
+		<li>Java <i>(sometimes)</i></li>
 	</ul>
 </section>
 
 <section>
 	<h2>Software I Like</h2>
 	<ul>
-		<li>Kitty Term</li>
-		<li>NeoVim</li>
+		<li>Neovim</li>
 		<li><i>software that sucks less ;)</i></li>
-		<li>Jetbrains IDEs <i>(i am a massive shill)</i></li>
+        <li>Autodesk Fusion</li>
 	</ul>
 </section>
 
@@ -64,4 +76,8 @@
 		<li><a href="https://github.com/mazylol">Github</a></li>
 		<li><a href="https://twitter.com/mazylol">Twitter</a></li>
 	</ul>
+</section>
+
+<section>
+    <p style="text-indent: 100%; white-space: nowrap; overflow: hidden;">DROP TABLE production;</p>
 </section>
