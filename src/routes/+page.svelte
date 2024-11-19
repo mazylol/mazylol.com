ecabd04ae6f831067376dfe866dfc3a6ef933735
Landon <landonp95@gmail.com>
2024-11-19 18:29:39 -0500

    fix formatter config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,9 @@
 <section>
 	<h2>What I Do <i>sometimes</i></h2>
 	<p>
-		I am a <b>{getAge('2006-09-05')}</b> year old student that likes to do pretty much everything. Jack of all trades, master of none. I started making discord bots with python during the pandemic and things kind of just went from there.
+		I am a <b>{getAge('2006-09-05')}</b> year old student that likes to do pretty much everything. Jack
+		of all trades, master of none. I started making discord bots with python during the pandemic and
+		things kind of just went from there.
 	</p>
 </section>
 
@@ -32,15 +34,22 @@
 			<li><a href="https://github.com/mazylol/kodi">Kodi</a> (programming info bot) (C++)</li>
 			<li><a href="https://github.com/mazylol/fido">Fido</a> (random dog pictures) (Rust)</li>
 			<li>
-				<a href="https://github.com/mazylol/discord.yaml">discord.yaml</a> (build a bot in completely in yaml)
-				(C++)
+				<a href="https://github.com/mazylol/discord.yaml">discord.yaml</a> (build a bot in completely
+				in yaml) (C++)
 			</li>
 		</ul>
-        <li>Websites</li>
-        <ul>
-            <li><a href="https://mazylol.com"><i>this one</i></a> (<a href="https://github.com/mazylol/mazylol.com">source</a>)</li>
-            <li><a href="https://esn.mazylol.com">ESN</a>, a website that hosts content produced by my schools sports network</li>
-        </ul>
+		<li>Websites</li>
+		<ul>
+			<li>
+				<a href="https://mazylol.com"><i>this one</i></a> (<a
+					href="https://github.com/mazylol/mazylol.com">source</a
+				>)
+			</li>
+			<li>
+				<a href="https://esn.mazylol.com">ESN</a>, a website that hosts content produced by my
+				schools sports network
+			</li>
+		</ul>
 		<li>Libraries</li>
 		<ul>
 			<li>
@@ -51,9 +60,10 @@
 				<a href="https://github.com/mazylol/ultimate-bogo">ultimate-bogo</a>, a multithreaded
 				bogosort implementation in C++ (high risk, high reward)
 			</li>
-            <li>
-                <a href="https://github.com/mazylol/twelvedata-cpp">twelvedata-cpp</a>, a request handler and deserializer for <a href="https://twelvedata.com">Twelvedata</a>
-            </li>
+			<li>
+				<a href="https://github.com/mazylol/twelvedata-cpp">twelvedata-cpp</a>, a request handler
+				and deserializer for <a href="https://twelvedata.com">Twelvedata</a>
+			</li>
 		</ul>
 		<li>*NIX</li>
 		<ul>
@@ -64,11 +74,14 @@
 		<li>3D Modeling/Printing</li>
 		<ul>
 			<li>
-				I made a DC power source for my schools robotics team. Encased in a model of my own design. Hardware is two lead-acid batteries, dc-dc buck converter, switch, xt-60 connectors, and some hot glue
+				I made a DC power source for my schools robotics team. Encased in a model of my own design.
+				Hardware is two lead-acid batteries, dc-dc buck converter, switch, xt-60 connectors, and
+				some hot glue
 			</li>
-            <li>
-                Got tired of cranking my coffee grinder. Made a coupling for it that attaches to a 12v motor I took from a traxxas rc car
-            </li>
+			<li>
+				Got tired of cranking my coffee grinder. Made a coupling for it that attaches to a 12v motor
+				I took from a traxxas rc car
+			</li>
 		</ul>
 	</ul>
 </section>
@@ -79,8 +92,8 @@
 		<li>C/C++</li>
 		<li>Go</li>
 		<li>Rust</li>
-        <li>Java/Kotlin</li>
-        <li>JS/TS</li>
+		<li>Java/Kotlin</li>
+		<li>JS/TS</li>
 	</ul>
 </section>
 
@@ -88,14 +101,14 @@
 	<h2>Software I Like</h2>
 	<ul>
 		<li><a href="https://neovim.io">Neovim</a></li>
-        <li><a href="https://hyprland.org">Hyprland</a></li>
+		<li><a href="https://hyprland.org">Hyprland</a></li>
 	</ul>
 </section>
 
 <section>
 	<h2>Links</h2>
 	<ul>
-        <li><a href="https://youtube.com/@bitwhyze">YouTube</a></li>
+		<li><a href="https://youtube.com/@bitwhyze">YouTube</a></li>
 		<li><a href="https://github.com/mazylol">Github</a></li>
 		<li><a href="https://twitter.com/mazylol">Twitter</a></li>
 	</ul>
